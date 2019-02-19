1f79aa4291ac5e0611108e83759a62efff197b85
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-02-19 12:55:49 -0500

    react native styled components 18:30

---
+++ components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.View`
+  background: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+`;
+
+const Card = props => {
+  return <Container />;
+};
+
+export default Card;
