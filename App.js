07c164831050637cf2a70c9149e142d827df75a7
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-02-21 08:14:09 -0500

    props and icons

--- App.js
+++ App.js
@@ -1,48 +1,72 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, SafeAreaView } from "react-native";
 import styled from "styled-components";
 import Card from "./components/Card";
+import { Icon } from "expo";
+import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <TitleBar>
-          <Avatar source={require("./assets/avatar.jpg")} />
-          <Title>Welcome back,</Title>
-          <Name>Meng</Name>
-        </TitleBar>
-        <Subtitle>Continue Learning</Subtitle>
-        <Card />
+        <SafeAreaView style={{ flex: 1 }}>
+          <ScrollView>
+            <TitleBar>
+              <Avatar source={require("./assets/avatar.jpg")} />
+              <Title>Welcome back,</Title>
+              <Name>Meng</Name>
+              <NotificationIcon
+                style={{ position: "absolute", right: 20, top: 5 }}
+              />
+            </TitleBar>
+            <ScrollView
+              horizontal={true}
+              style={{
+                flexDirection: "row",
+                padding: 20,
+                paddingLeft: 12,
+                paddingTop: 30
+              }}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Logo
+                image={require("./assets/logo-framerx.png")}
+                text="Framer X"
+              />
+              <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+            </ScrollView>
+            <Subtitle>Continue Learning</Subtitle>
+            <ScrollView
+              horizontal={true}
+              style={{ paddingBottom: 30 }}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Card
+                title="Styled Components"
+                image={require("./assets/background2.jpg")}
+                logo={require("./assets/logo-react.png")}
+                caption="React Native"
+                subtitle="5 of 12 sections"
+              />
+              <Card
+                title="Styled Components 2"
+                image={require("./assets/background2.jpg")}
+                logo={require("./assets/logo-react.png")}
+                caption="React Native"
+                subtitle="5 of 12 sections"
+              />
+            </ScrollView>
+          </ScrollView>
+        </SafeAreaView>
       </Container>
     );
   }
 }
 
-const Subtitle = styled.Text`
-  color: #b8bece;
-  font-weight: 600;
-  font-size: 15px;
-  margin-left: 20px;
-  margin-top: 50px;
-  text-transform: uppercase;
-`;
-
 const Container = styled.View`
   flex: 1;
   background-color: #f0f3f5;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  color: #b8bece;
-  font-weight: 500;
-`;
-
-const Name = styled.Text`
-  font-size: 20px;
-  color: #3c4560;
-  font-weight: bold;
 `;
 
 const TitleBar = styled.View`
@@ -60,4 +84,25 @@ const Avatar = styled.Image`
   position: absolute;
   top: 0;
   left: 0;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 20px;
+  text-transform: uppercase;
 `;
--- components/Card.js
+++ components/Card.js
@@ -1,15 +1,87 @@
 import React from "react";
 import styled from "styled-components";
 
+const Card = props => {
+  return (
+    <Container>
+      <Cover>
+        <Image source={props.image} />
+        <Title>{props.title}</Title>
+      </Cover>
+      <Content>
+        <Logo source={props.logo} />
+        <Wrapper>
+          <Caption>{props.caption}</Caption>
+          <Subtitle>{props.subtitle}</Subtitle>
+        </Wrapper>
+      </Content>
+    </Container>
+  );
+};
+
+export default Card;
+
 const Container = styled.View`
   background: white;
   width: 315px;
   height: 280px;
   border-radius: 14px;
+  margin-left: 20px;
+  margin-top: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
 `;
 
-const Card = props => {
-  return <Container />;
-};
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
 
-export default Card;
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  margin: 20px;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  width: 170px;
+`;
+
+const Content = styled.View`
+  padding-left: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 80px;
+`;
+
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+`;
+
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
+
+const Caption = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  text-transform: uppercase;
+  margin-top: 4px;
+`;
